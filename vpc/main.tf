ff8a9c4b4ff7a7b375a897fb8787a95ed589a332
Chris <chris.hickey@vidence.com>
2022-11-19 11:32:22 -0700

    adding in the vpc module stuff

---
+++ vpc/main.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "buzser-dev" {
+  cidr_block = var.main_vpc_cidr 
+  instance_tenancy = "default"
+}
+
+resource "aws_internet_gateway" "buzser-dev-igw" {
+    vpc_id = aws_vpc.buzser-dev.id
+}
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.buzser-dev.id 
+    cidr_block = "var.public_subnets"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.buzser-dev.id
+  cidr_block = "var.private_subnet"
+}
+
+resource "aws_route_table" "BuzserPublicRT" {
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.buzser-dev-igw.id
+  }
+}
+resource "aws_route_table" "BuzserPrivateRT" {
+  vpc_id = aws_vpc.buzser-dev.id 
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_id
+  }
+}
+
+resource "aws_route_table_association" "PublicRTassociation" {
+  subnet_id = aws_subnet.public_subnet
+  route_table_id = aws_route_table.BuzserPublicRT
+}
+
+resource "aws_route_table" "name" {
+  subnet_id = aws_subnet.private_subnet
+  route_table_id = aws_route_table.BuzserPrivateRT
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  variable "main_vpc_cidr" {
+
+  }
+  variable "public_subnets" {
+
+  }
+  variable "private_subnets" {
+    
+  }
+}

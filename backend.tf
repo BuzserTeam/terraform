752625e4b95edcdfdcf97751fb91433ab8173d79
Chris Hickey <chrismhickey@gmail.com>
2025-05-22 15:05:53 -0600

    ran terraform fmt

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "buzser-terraform-state"
     key            = "terraform/state/buzser.tfstate"
-    region         = "us-east-1"  # Adjust to your preferred region
+    region         = "us-east-1" # Adjust to your preferred region
     dynamodb_table = "buzser-terraform-locks"
     encrypt        = true
   }
